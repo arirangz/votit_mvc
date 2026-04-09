4fff179c740ef11d086428b92b7d37f82f9e1f17
Anthony L <laplane_anthony@yahoo.fr>
2026-04-09 11:09:28 +0200

    fix mysql

--- src/Db/Mysql.php
+++ src/Db/Mysql.php
@@ -8,7 +8,15 @@ class Mysql {
     private $pdo;
 
     private function __construct() {
-        $this->pdo = new PDO('mysql:host=localhost;dbname=studi_votit;charset=utf8', 'root', '');
+
+        $dbConf = parse_ini_file(APP_ROOT."/".APP_ENV);
+        $dbHost = $dbConf["db_host"];
+        $dbName = $dbConf["db_name"];
+        $dbUser = $dbConf["db_user"];
+        $dbPassword = $dbConf["db_password"];
+        $dbPort = $dbConf["db_port"];
+    
+        $this->pdo = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
